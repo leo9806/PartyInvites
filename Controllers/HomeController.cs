1bd80caa93962adb9d4337e8b952e1bb6ec8308f
leo <antoniou.leo@gmail.com>
2020-09-09 11:20:11 +0300

    Added a data model for GuestResponse

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,31 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PartyInvites.Models;
 
-namespace PartyInvites.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index () {
+            return View ();
         }
     }
 }
